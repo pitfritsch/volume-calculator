77f12162afb58892b619d90c6f38df5d2f3207f5
Pedro Fritsch <pedrofritsch98@gmail.com>
2022-10-13 14:41:46 +1000

    Cube and sphere volume calculator

--- App.tsx
+++ App.tsx
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NativeBaseProvider } from "native-base";
+import React from "react";
+import Home from "./pages/Home";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="home">
+          <Stack.Screen
+            name="home"
+            component={Home}
+            options={{ title: "Volume calculator" }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ pages/Home.tsx
@@ -0,0 +1,134 @@
+import {
+  Box,
+  Button,
+  Center,
+  Flex,
+  FormControl,
+  Input,
+  Menu,
+  Select,
+  Switch,
+  Text,
+  useToast,
+} from "native-base";
+import React, { useEffect, useState } from "react";
+import { StyleSheet } from "react-native";
+
+type PossibleShapes = "cube" | "sphere";
+
+function isNumber(n: string | number) {
+  return Number(n) == n;
+}
+
+function CubeForm() {
+  const toast = useToast();
+
+  const [length, setLength] = useState<string>("");
+  const [width, setWidth] = useState<string>("");
+  const [height, setHeight] = useState<string>("");
+
+  const [volume, setVolume] = useState<number>(0);
+
+  useEffect(() => {
+    toast.closeAll();
+    if (!isNumber(length) || !isNumber(width) || !isNumber(height)) {
+      toast.show({
+        description: "Some value has a non-number value",
+        placement: `top`,
+      });
+    }
+    setVolume(Number(length) * Number(width) * Number(height));
+  }, [length, width, height]);
+
+  return (
+    <Box>
+      <FormControl>
+        <FormControl.Label>Length</FormControl.Label>
+        <Input value={length} onChangeText={setLength} />
+      </FormControl>
+      <FormControl>
+        <FormControl.Label>Width</FormControl.Label>
+        <Input value={width} onChangeText={setWidth} />
+      </FormControl>
+      <FormControl>
+        <FormControl.Label>Height</FormControl.Label>
+        <Input value={height} onChangeText={setHeight} />
+      </FormControl>
+      <Text textAlign="right" marginTop="2" color="#7c7c7c">
+        * All sizes in cm
+      </Text>
+      <Center height="100px">
+        <Text fontSize={30}>{volume} cm³</Text>
+      </Center>
+    </Box>
+  );
+}
+
+function SphereForm() {
+  const toast = useToast();
+  const [radius, setRadius] = useState<string>("");
+
+  const [volume, setVolume] = useState<number>(0);
+
+  useEffect(() => {
+    toast.closeAll();
+    if (!isNumber(radius)) {
+      toast.show({
+        description: "Some value has a non-number value",
+        placement: `top`,
+      });
+    }
+    const numberedRadius = Number(radius);
+
+    setVolume(Math.round((4 / 3) * Math.PI * Math.pow(numberedRadius, 3)));
+  }, [radius]);
+
+  return (
+    <Box>
+      <FormControl>
+        <FormControl.Label>Radius</FormControl.Label>
+        <Input value={radius} onChangeText={setRadius} />
+      </FormControl>
+      <Text textAlign="right" marginTop="2" color="#7c7c7c">
+        * All sizes in cm
+      </Text>
+      <Center height="100px">
+        <Text fontSize={30}>{volume} cm³</Text>
+      </Center>
+    </Box>
+  );
+}
+
+const Forms: { [key in PossibleShapes]: () => JSX.Element } = {
+  cube: CubeForm,
+  sphere: SphereForm,
+};
+
+export default function Home() {
+  const [selectedShape, setSelectedShape] = useState<PossibleShapes>("cube");
+
+  const Form = Forms[selectedShape];
+
+  return (
+    <Flex padding="10">
+      <Menu
+        trigger={(triggerProps) => (
+          <Button {...triggerProps}>{selectedShape}</Button>
+        )}
+      >
+        <Menu.Item onPress={() => setSelectedShape("cube")}>Cube</Menu.Item>
+        <Menu.Item onPress={() => setSelectedShape("sphere")}>Sphere</Menu.Item>
+      </Menu>
+      <Form />
+    </Flex>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
